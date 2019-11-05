e588079fed10f126ca17995c8ab780502e982dbf
Tom <tomdriscoll5@gmail.com>
2019-11-05 12:24:16 -0500

    leve slider displays level-value

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const GameView = require("./game_view")
 const Pill = require("./pill")
 
-document.addEventListener("DOMContentLoaded", function () {``
+
+
+document.addEventListener("DOMContentLoaded", function () {
     var vid = document.getElementById("fever");
     vid.volume = 0.0;
+    document.getElementById('level-slider').addEventListener( "mouseup", function () {
+      document.getElementById('level-value').textContent =  document.getElementById('level-slider').value
+    })
     new GameView().splash();
   });
   
